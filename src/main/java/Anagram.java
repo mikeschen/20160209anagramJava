31dd84cd65d60f2cfcee99f89d7e77a0f62f2eb0
Michelle Brecunier and Mike Schennum <mikeschenphoto@gmail.com>
2016-02-09 11:51:06 -0800

    add initial anagram files

---
+++ src/main/java/Anagram.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class Anagram {
+  public static void main(String[] args) {
+  }
+
+  public static Boolean isAnagram() {
+  }
+
+}
