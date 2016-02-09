cbdfc43c796ac80987b45ebee485fbba6e9c0495
Michelle Brecunier and Mike Schennum <michelle.brecunier@gmail.com>
2016-02-09 14:19:28 -0800

    compares two word arrays

--- src/main/java/Anagram.java
+++ src/main/java/Anagram.java
@@ -4,12 +4,21 @@ import java.util.ArrayList;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+import java.util.Arrays;
 
 public class Anagram {
   public static void main(String[] args) {
   }
 
-  public static Boolean isAnagram() {
+  public static Boolean isAnagram(String word1, String word2) {
+    char[] word1Array = word1.toLowerCase().toCharArray();
+    char[] word2Array = word2.toLowerCase().toCharArray();
+    Arrays.sort(word1Array);
+    Arrays.sort(word2Array);
+    if (Arrays.equals(word1Array, word2Array)) {
+      return true;
+    } else {
+      return false;
+    }
   }
-
 }
--- src/test/java/AnagramTest.java
+++ src/test/java/AnagramTest.java
@@ -3,5 +3,15 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 
 public class AnagramTest {
+  @Test
+  public void isAnagram_WillReturnFalseIfNotAnagram() {
+    Anagram testAnagram = new Anagram();
+    assertEquals(false, testAnagram.isAnagram("cat", "doggy"));
+  }
 
+  @Test
+  public void isAnagram_WillReturnTrueIfAnagram() {
+    Anagram testAnagram = new Anagram();
+    assertEquals(true, testAnagram.isAnagram("cat", "tac"));
+  }
 }
